c6dea01a94394edc5e64872a378708ff5c5efcc4
Вадим <vadim.a.shkuratov@gmail.com>
2021-07-11 16:11:10 +0300

    Add asp.net project. Add model database, context

---
+++ README.md
@@ -0,0 +1,2 @@
+# TelcoTest
+ Test task from EpolSoft. The task contains a console (C#) and a web application (Asp.Net Core 5, MVC 5)
---
+++ TelcoTestAsp/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TelcoTestAsp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Start_date { get; set; }
+        public DateTime End_date { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+}
